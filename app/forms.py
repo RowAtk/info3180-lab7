056c0eced1dd8485c63c9c7cad98f42c07c45cfa
rowatk <rowaneatkinson@gmail.com>
2020-05-03 20:34:45 -0500

    created upload form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, FileField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileRequired, FileAllowed
 
+nullmsg = "This field is required"
+
+class UploadForm(FlaskForm):
+    """ Form for file upload """
+    description = TextAreaField(
+        label="Description", 
+        validators=[
+            DataRequired(message=nullmsg),
+        ]
+    )
+
+    photo = FileField(
+        label="Upload Picture", 
+        validators=[ 
+            FileAllowed(
+                ['jpg', 'png', 'jpeg'], 
+                message="Image Files Only"
+            )
+        ]
+    )
